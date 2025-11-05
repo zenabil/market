e2249c3ded327362ebaf71250a2194cb9135b0fb
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-05 00:55:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-348306126-eba01",
+  "appId": "1:31870417013:web:97661b130891085b22e03c",
+  "apiKey": "AIzaSyAfT_f-RsTLvL-81Olnh1NI9dj_L9SgTLY",
+  "authDomain": "studio-348306126-eba01.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "31870417013"
+};
