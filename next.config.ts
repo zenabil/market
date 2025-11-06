239253a0d4bfb8d6064c4cfae573e585c6f73a3b
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-06 14:54:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,9 @@ const nextConfig: NextConfig = {
   experimental: {
     // This is required to allow the Next.js dev server to be accessed from the
     // Cloud Workstation's forwarded port.
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1762300038578.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev',
-    ],
+    // allowedDevOrigins: [
+    //   'https://6000-firebase-studio-1762300038578.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev',
+    // ],
   },
   typescript: {
     ignoreBuildErrors: true,
