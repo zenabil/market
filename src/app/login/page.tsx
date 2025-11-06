c3333d51ae9ea3f0e36e3082682f703ec476507a
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-06 23:28:54 +0000

    اريد ان يكون الموقع سريع الاستجابة

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -236,26 +236,15 @@ export default function LoginPage() {
 
   if (isUserLoading || isRoleLoading || user) {
     return (
-        <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-            <div className="flex items-center justify-center py-12">
-                 <Loader2 className="h-8 w-8 animate-spin text-primary" />
-            </div>
-            <div className="hidden bg-muted lg:block">
-                <Image
-                    src="https://images.unsplash.com/photo-1588964895597-cf29a3a954a2?q=80&w=1974&auto=format&fit=crop"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-            </div>
+        <div className="w-full min-h-screen flex items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-primary" />
         </div>
     );
   }
 
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:grid-cols-5">
+       <div className="flex items-center justify-center py-12 lg:col-span-1 xl:col-span-2">
          <div className='absolute top-8 left-8'>
             <Link href="/" >
               <Logo className="h-8" />
@@ -453,9 +442,9 @@ export default function LoginPage() {
             </Tabs>
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden lg:block lg:col-span-1 xl:col-span-3">
         <Image
-          src="https://images.unsplash.com/photo-1588964895597-cf29a3a954a2?q=80&w=1974&auto=format&fit=crop"
+          src="https://images.unsplash.com/photo-1543168256-418811576931?q=80&w=1974&auto=format&fit=crop"
           alt="Image d'une épicerie avec des produits frais"
           width="1920"
           height="1080"
