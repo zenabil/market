ac33ec54191b20381825e13b1213d48f767abb81
zenagui bilal <zenaguibilal2@gmail.com>
2025-11-07 19:49:44 +0000

    اكمل مُبدّل اللغةاكمل مُبدّل اللغة

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
